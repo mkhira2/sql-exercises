40e3f0e0278f7b2d205fe12c51f04aa535465fdc
Kenji Hirabayashi <mkhira2@gmail.com>
2017-06-15 09:19:19 -0500

    04 setup complete

--- 04-sql-exercise/04-exercises.sql
+++ 04-sql-exercise/04-exercises.sql
@@ -1,1 +1,30 @@
 -- LINK: https://en.wikibooks.org/wiki/SQL_Exercises/The_computer_store
+
+-- Select the title of all movies.
+
+
+-- Show all the distinct ratings in the database.
+
+
+-- Show all unrated movies.
+
+
+-- Select all movie theaters that are not currently showing a movie.
+
+
+-- Select all data from all movie theaters and, additionally, the data from the movie that is being shown in the theater (if one is being shown).
+
+
+-- Select all data from all movies and, if that movie is being shown in a theater, show the data from the theater.
+
+
+-- Show the titles of movies not currently being shown in any theaters.
+
+
+-- Add the unrated movie "One, Two, Three".
+
+
+-- Set the rating of all unrated movies to "G".
+
+
+-- Remove movie theaters projecting movies rated "NC-17".
