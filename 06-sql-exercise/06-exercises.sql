2d01db1c63c03e03364b05e3e77dab9d0a896421
Kenji Hirabayashi <mkhira2@gmail.com>
2017-06-17 10:55:24 -0500

    finished 06-exercises

--- 06-sql-exercise/06-exercises.sql
+++ 06-sql-exercise/06-exercises.sql
@@ -1,1 +1,4 @@
 -- LINK: https://en.wikibooks.org/wiki/SQL_Exercises/The_computer_store
+
+-- List all the scientists' names, their projects' names, and the hours worked by that scientist on each project, in alphabetical order of project name, then scientist name.
+SELECT scientists.name, projects.name, projects.hours FROM scientists INNER JOIN assignedto ON scientists.ssn = assignedto.scientist INNER JOIN projects ON assignedto.project = projects.code ORDER BY projects.name ASC, scientists.name ASC;
